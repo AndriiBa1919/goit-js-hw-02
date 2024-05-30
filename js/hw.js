7d0a7621e3813caf3e00e8fdf9518716d70fc4c3
AndriiBa1919 <159924402+AndriiBa1919@users.noreply.github.com>
2024-05-30 16:45:05 +0300

    Update hw.js

--- js/hw.js
+++ js/hw.js
@@ -1,238 +1,36 @@
-// const getUserNames = (users) => {
-//   return users.map(user => user.name)
-// }
+// const pizzaPalace = {
+//   pizzas: ["Supercheese", "Smoked", "Four meats"],
+//   checkPizza(pizzaName) {
+//     return pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = checkPizza(pizzaName);
 
-// console.log(
-//   getUserNames([
-//   {
-//     name: "Moore Hensley",
-//     email: "moorehensley@indexia.com",
-//     balance: 2811
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     email: "sharlenebush@tubesys.com",
-//     balance: 3821
-//   },
-//   {
-//     name: "Ross Vazquez",
-//     email: "rossvazquez@xinware.com",
-//     balance: 3793
-//   },
-//   {
-//     name: "Elma Head",
-//     email: "elmahead@omatom.com",
-//     balance: 2278
-//   },
-//   {
-//     name: "Carey Barr",
-//     email: "careybarr@nurali.com",
-//     balance: 3951
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     email: "blackburndotson@furnigeer.com",
-//     balance: 1498
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     email: "shereeanthony@kog.com",
-//     balance: 2764
-//   },
-// ])
-// );
-//!-------------------------------------
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
 
-// const allUsers = [
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
 //   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//   },
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//   },
-// ];
-
-// const getUsersWithFriend = (allUsers, friendName) => {
-//   return allUsers.filter((user) => (user.friends.includes(friendName)));
 // };
 
-// console.log(getUsersWithFriend(allUsers, "Briana Decker"));
-// // [
-// //   {
-// //     name: "Sharlene Bush",
-// //     friends: ["Briana Decker", "Sharron Pace"]
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"]
-// //   }
-// // ]
+// console.log(pizzaPalace.order("Four meats"));
+class Car {
+  static #maxPrice = 50000;
+  static checkPrice(price) {
+    if (price >= Car.#maxPrice) {
+      return "Error! Price exceeds the maximum";
+    }
+    return "Success! Price is within acceptable limits";
+  }
 
-// console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// // [
-// //   {
-// //     name: "Elma Head",
-// //     friends: ["Goldie Gentry", "Aisha Tran"]
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"]
-// //   }
-// // ]
+  constructor(params) {
+    this.price = params.price;
+  }
+}
 
-// console.log(getUsersWithFriend(allUsers, "Adrian Cross")); // []
-//!---------------------------------------------------------
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
 
-// const sortByDescendingFriendCount = (users) => {
-//   return users.toSorted((a, b) => b.friends.length - a.friends.length)
-// }
-
-// console.log(
-//   sortByDescendingFriendCount([
-//     {
-//       name: "Moore Hensley",
-//       friends: ["Sharron Pace"],
-//       gender: "male"
-//     },
-//     {
-//       name: "Sharlene Bush",
-//       friends: ["Briana Decker", "Sharron Pace"],
-//       gender: "female"
-//     },
-//     {
-//       name: "Ross Vazquez",
-//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//       gender: "male"
-//     },
-//     {
-//       name: "Elma Head",
-//       friends: ["Goldie Gentry", "Aisha Tran"],
-//       gender: "female"
-//     },
-//     {
-//       name: "Carey Barr",
-//       friends: ["Jordan Sampson", "Eddie Strong"],
-//       gender: "male"
-//     },
-//     {
-//       name: "Blackburn Dotson",
-//       friends: ["Jacklyn Lucas", "Linda Chapman"],
-//       gender: "male"
-//     },
-//     {
-//       name: "Sheree Anthony",
-//       friends: ["Goldie Gentry", "Briana Decker"],
-//       gender: "female"
-//     }
-//   ])
-// );
-// // [
-// //   {
-// //     name: "Ross Vazquez",
-// //     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-// //     gender: "male"
-// //   },
-// //   {
-// //     name: "Sharlene Bush",
-// //     friends: ["Briana Decker", "Sharron Pace"],
-// //     gender: "female"
-// //   },
-// //   {
-// //     name: "Elma Head",
-// //     friends: ["Goldie Gentry", "Aisha Tran"],
-// //     gender: "female"
-// //   },
-// //   {
-// //     name: "Carey Barr",
-// //     friends: ["Jordan Sampson", "Eddie Strong"],
-// //     gender: "male"
-// //   },
-// //   {
-// //     name: "Blackburn Dotson",
-// //     friends: ["Jacklyn Lucas", "Linda Chapman"],
-// //     gender: "male"
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"],
-// //     gender: "female"
-// //   },
-// //   {
-// //     name: "Moore Hensley",
-// //     friends: ["Sharron Pace"],
-// //     gender: "male"
-// //   }
-// // ]
-//!----------------------------------------------------
-
-const clients = [
-  {
-    name: "Moore Hensley",
-    gender: "male",
-    balance: 2811,
-  },
-  {
-    name: "Sharlene Bush",
-    gender: "female",
-    balance: 3821,
-  },
-  {
-    name: "Ross Vazquez",
-    gender: "male",
-    balance: 3793,
-  },
-  {
-    name: "Elma Head",
-    gender: "female",
-    balance: 2278,
-  },
-  {
-    name: "Carey Barr",
-    gender: "male",
-    balance: 3951,
-  },
-  {
-    name: "Blackburn Dotson",
-    gender: "male",
-    balance: 1498,
-  },
-  {
-    name: "Sheree Anthony",
-    gender: "female",
-    balance: 2764,
-  },
-];
-
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter((user) => user.gender === gender)
-    .reduce((acc, user) => {
-      return acc + user.balance;
-    }, 0);
-};
-
-console.log(getTotalBalanceByGender(clients, "male")); // 12053
-
-console.log(getTotalBalanceByGender(clients, "female")); // 8863
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
